bfbf1a832ac9f540f8e2a3df814e751c5bafa7dc
m.stavrov <m.stavrov@vk.team>
2023-11-27 13:53:00 +0300

    fix(slider.js): устранил утечку памяти

--- src/modules/slider/slider.js
+++ src/modules/slider/slider.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import './slider.css';
 
 function Slider({ slides, page }) {
   let [ currentImageIndex, setCurrentImageIndex ] = useState(1);
   let [ containerHeight, setContainerHeight ] = useState('');
+  const playerContainerRef = useRef(null);
 
   function switchSlide(event) {
     const direction = event.currentTarget.dataset.direction;
@@ -26,19 +27,23 @@ function Slider({ slides, page }) {
     })
   }
 
-  function handleSliderResize(e) {
-    const playerContainer = document.querySelector('.slider__image');
-    const containerHeight = playerContainer.offsetWidth / 1.56;
+  const handleSliderResize = useCallback(() => {
+    const containerHeight = playerContainerRef.current.offsetWidth / 1.56;
     setContainerHeight(containerHeight);
-  }
+  }, [])
 
-  window.addEventListener('resize', handleSliderResize);
+  useEffect(() => {
+    handleSliderResize()
+    window.addEventListener('resize', handleSliderResize);
 
-  useEffect(handleSliderResize);
+    return () => {
+      window.removeEventListener('resize', handleSliderResize);
+    }
+  }, [handleSliderResize]);
 
   return (
     <div className='slider'>
-      <div className='slider__image' src={`./images/slider/${slides[currentImageIndex - 1]}`} alt='slide' style={{ height: containerHeight, backgroundImage: `url(./images/slider/${slides[currentImageIndex - 1]})` }}/>
+      <div className='slider__image' ref={playerContainerRef} src={`./images/slider/${slides[currentImageIndex - 1]}`} alt='slide' style={{ height: containerHeight, backgroundImage: `url(./images/slider/${slides[currentImageIndex - 1]})` }}/>
       <div className='slider__buttons'>
         <img className='slider__button-left' src='./images/global/left.svg' data-direction="prev" onClick={switchSlide} alt='left-button' />
         <img className='slider__button-right' src='./images/global/right.svg' data-direction="next" onClick={switchSlide} alt='right-button' />
